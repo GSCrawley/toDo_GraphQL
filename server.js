a01fe9fc8f10ad6a436f7d610e8c757628312f3d
Gideon Crawley <gscrawley@gmail.com>
2021-05-13 21:46:53 -0700

    added app and assigned port

--- server.js
+++ server.js
@@ -66,3 +66,17 @@ const root = {
         return todo;
     }
 }
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+}))
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Running on port: ${port}`)
+})
+
