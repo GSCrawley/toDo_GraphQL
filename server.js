fc31656bf3391279867f06245ad44d2b3f7110ee
Gideon Crawley <gscrawley@gmail.com>
2021-05-13 21:42:05 -0700

    changed schema to include completeTodo mutation

--- server.js
+++ server.js
@@ -2,25 +2,33 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
-let todos =[
-    {id: 1, description:'Dont forget your laundry', owner: 'Gideon Crawley', completed: false, active: true}
-];
 
-const typeDefs = `
-    type Query{
-        info: String!
-        allToDos: [ToDo!]!
-    }
-    type Mutation{
-        createToDo(description: String!, owner: String!,  completed: Boolean!, active: Boolean!): ToDo!
-        updateToDo(id: Int!, description: String!, owner: String!, completed: Boolean!, active: Boolean!): ToDo!
-        deleteToDo(id: Int!): ToDo!
-    }
-    type ToDo{
-        id: Int!
-        description: String! 
-        owner: String! 
+
+const schema = buildSchema(`
+    type Todo {
+
+        name: String!
         completed: Boolean!
-        active: Boolean!
+        date: String!
+        id: Int!
     }
-`;
+    type Query {
+        getAllTodos: [Todo!]!
+        getTodo(id: Int!): Todo!
+        getCompletedTodos: [Todo!]!
+    }
+    type Mutation {
+        addTodo(name: String!): Todo!
+        completeTodo(id: Int!): Todo!
+    }
+`)
+
+const toDoList = [];
+
+const resolvers ={
+    Query:{
+        info: ()=> `Todays Tasks`,
+        allToDos: async(parent, args,context)=>{            
+            return context.prisma.toDo.findMany();
+        }
+    },
